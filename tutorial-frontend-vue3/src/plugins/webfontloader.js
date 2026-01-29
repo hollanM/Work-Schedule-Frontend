75f4a0e13739ed69ace1ab8727bb5273657ce82c
hollanM <122317608+hollanM@users.noreply.github.com>
2026-01-29 16:25:12 -0600

    Delete tutorial-frontend-vue3 directory

--- tutorial-frontend-vue3/src/plugins/webfontloader.js
+++
@@ -1,9 +0,0 @@
-import { load } from "webfontloader";
-
-export async function loadFonts() {
-  load({
-    google: {
-      families: ["Roboto:100,300,400,500,700,900&display=swap"],
-    },
-  });
-}
