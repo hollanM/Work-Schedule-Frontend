a104a82ee68115b480057d68f0ecc2bfbcf18c18
Austin Neubauer <austin.neubauer@eagles.oc.edu>
2026-02-16 21:33:50 -0600

    Bar is coming along but this is everything I got done today, got images, words, colors, and I am working on spacing and onHover effects

---
+++ src/components/BurgerIcon.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  size: { type: [Number, String], default: 32 },
+  color: { type: String, default: 'currentColor' },
+  strokeWidth: { type: [Number, String], default: 2 }
+})
+</script>
+
+<template>
+  <svg class ="left-margin"
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    role="img"
+  >
+    <path d="M4 18L20 18" :stroke="color" :stroke-width="strokeWidth" stroke-linecap="round"/>
+    <path d="M4 12L20 12" :stroke="color" :stroke-width="strokeWidth" stroke-linecap="round"/>
+    <path d="M4 6L20 6" :stroke="color" :stroke-width="strokeWidth" stroke-linecap="round"/>
+  </svg>
+</template>
--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,5 +1,11 @@
 <script setup>
 import ocLogo from "/oc-logo-white.png";
+import Attendance from "/Attendance.png";
+import Bell from "/Bell.png";
+import Dashboard from "/Dashboard.png";
+import Inbox from "/Inbox.png";
+import Schedule from "/Schedule.png";
+import Settings from "/Settings.png";
 import { ref, onMounted } from "vue";
 import Utils from "../config/utils";
 import AuthServices from "../services/authServices";
@@ -40,20 +46,39 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <v-app-bar app>
-      <router-link :to="{ name: 'tutorials' }">
-        <v-img
-          class="mx-2"
-          :src="logoURL"
-          height="50"
-          width="50"
-          contain
-        ></v-img>
-      </router-link>
-      <v-toolbar-title class="title">
-        {{ title }}
-      </v-toolbar-title>
+  <div app>
+    <v-app-bar id="app-bar">
+      <div id="Bell_Div" v-if="user" class="container">
+        <div>
+          <router-link :to="{ name: '' }">
+            <v-img class="image" id="Bell_img" :src="Bell" height="50" width="50" contain></v-img>
+          </router-link>
+        </div>
+      </div>
+      <div id="Dashboard_Div" v-if="user" class="container">
+        <div>
+          <router-link :to="{ name: '' }">
+            <v-img class="image" id="Dashboard_img" :src="Dashboard" height="50" width="50" contain></v-img>
+          </router-link>
+        </div>
+        <p id="Dashboard_p"> DashBoard </p>
+      </div>
+      <div id="Schedule_Div" v-if="user" class="container">
+        <div flex="1">
+          <router-link :to="{ name: '' }">
+            <v-img class="image" id="Schedule_img" :src="Schedule" height="50" width="50" contain></v-img>
+          </router-link>
+        </div>
+        <p id="Schedule_p"> Schedule </p>
+      </div>
+      <div id="Attendance_Div" v-if="user" class="container">
+      <div>
+        <router-link :to="{ name: '' }">
+          <v-img class="image" id="Attendance_img" :src="Attendance" height="50" width="50" contain></v-img>
+        </router-link>
+      </div>
+      <p id="Attendance_"> Attendance </p>
+      </div>
       <v-spacer></v-spacer>
       <div v-if="user">
         <v-btn class="mx-2" :to="{ name: 'tutorials' }"> List </v-btn>
@@ -71,9 +96,7 @@ onMounted(() => {
           <v-card-text>
             <div class="mx-auto text-center">
               <v-avatar color="secondary" class="mt-2 mb-2">
-                <span class="accent--text font-weight-bold">{{
-                  initials
-                }}</span>
+                <span class="accent--text font-weight-bold">{{  initials  }}</span>
               </v-avatar>
               <h3>{{ name }}</h3>
               <p class="text-caption mt-1">
@@ -88,3 +111,45 @@ onMounted(() => {
     </v-app-bar>
   </div>
 </template>
+
+<style>
+.container 
+{
+  cursor: pointer; 
+}
+
+#app-bar 
+{
+  /*background-color: rgb(60, 60, 60);*/
+}
+
+#Bell_Div
+{
+  background-color: rgba(60, 60, 60);
+  color: rgb(193, 193, 193);
+}
+
+#Dashboard_Div
+{
+  background-color: rgba(60, 60, 60);
+  color: rgb(193, 193, 193);
+}
+
+
+#Dashboard_Div:hover
+{
+  filter: brightness(50%); 
+}
+
+#Schedule_Div
+{
+  background-color: rgba(60, 60, 60);
+  color: rgb(193, 193, 193);
+}
+
+#Attendance_Div
+{
+  background-color: rgba(60, 60, 60);
+  color: rgb(193, 193, 193);
+}
+</style>
