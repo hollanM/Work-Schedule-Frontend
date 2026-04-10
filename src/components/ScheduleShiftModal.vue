5b4ec85fa30845d264c3ff46045270e7788bf7fe
slammingsam <samuel.harris@eagles.oc.edu>
2026-04-10 14:22:05 -0500

    Add employee_name and date props to ScheduleShiftModal for shift editing

--- src/components/ScheduleShiftModal.vue
+++ src/components/ScheduleShiftModal.vue
@@ -12,6 +12,7 @@ import Utils from "../config/utils.js";
 import { useRouter } from "vue-router";
 import { VBtn } from "vuetify/components";
 
+// merge
 const props = defineProps({
   employee_name: { type: [Number, String], required: true },
   date: { type: String, required: true },
