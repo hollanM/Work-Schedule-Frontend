7c29eb104161deaae0a55876a5d9b8db511caf7f
hollanM <h.mcclendon@eagles.oc.edu>
2026-04-18 16:56:20 -0500

    Made weekly schedule services

---
+++ src/services/weekly_scheduleServices.js
@@ -0,0 +1,25 @@
+import apiClient from "./services.js";
+
+export default {
+  getAll() {
+    return apiClient.get("/weekly_schedules");
+  },
+  get(id) {
+    return apiClient.get(`/weekly_schedules/${id}`);
+  },
+   getForUser(id) {
+    return apiClient.get(`/weekly_schedules/users/${id}`);
+  },
+  create(data) {
+    return apiClient.post("/weekly_schedules", data);
+  },
+  update(id, data) {
+    return apiClient.put(`/weekly_schedules/${id}`, data);
+  },
+  delete(id) {
+    return apiClient.delete(`/weekly_schedules/${id}`);
+  },
+  deleteAll() {
+    return apiClient.delete(`/weekly_schedules`);
+  },
+};
